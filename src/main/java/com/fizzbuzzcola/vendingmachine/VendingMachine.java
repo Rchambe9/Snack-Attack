2bb3371110084da2f3e66238e7cddd98cbe532b7
Rchambe9 <Rchambe9@gmail.com>
2020-08-08 13:18:40 -0400

    Created product dispense and thank you message

--- src/main/java/com/fizzbuzzcola/vendingmachine/VendingMachine.java
+++ src/main/java/com/fizzbuzzcola/vendingmachine/VendingMachine.java
@@ -6,7 +6,10 @@ import java.util.Collection;
 public class VendingMachine {
 //    variable
     private BigDecimal currentAmount;
-    public Collection<Product> snacks;
+    private Collection<Product> snacks;
+    private Cola sampleCola = new Cola();
+    private Chips sampleChips = new Chips();
+    private Candy sampleCandy = new Candy();
 //    constructor
     public VendingMachine(BigDecimal currentAmount) {
         this.currentAmount = currentAmount;
@@ -33,29 +36,33 @@ public class VendingMachine {
         }
 
     }
-    public Product selectProduct(int choice){
-        if (choice == 1){
+    public Product selectProduct(int choice) {
+        if (choice == 1 && currentAmount.compareTo(sampleCola.getCost()) <= 0) {
+            System.out.println("Thank you.  Enjoy your Cola!");
             return new Cola();
-        } else if (choice == 2){
+        } else if (choice == 2 && currentAmount.compareTo(sampleChips.getCost()) <= 0) {
+            System.out.println("Thank you. Enjoy your Chips!");
             return new Chips();
-        } else if (choice == 3){
+        } else if (choice == 3 && currentAmount.compareTo(sampleCandy.getCost()) <= 0) {
+            System.out.println("Thank you. Enjoy your Candy!");
             return new Candy();
         }
+        return null;
     }
 
     public String acceptCoin(String coin) {
         BigDecimal dimeValue = new BigDecimal("0.10");
         BigDecimal nickelValue = new BigDecimal("0.05");
         BigDecimal quarterValue = new BigDecimal("0.25");
-            if (coin.equals("dime")){
-                currentAmount = currentAmount.add(dimeValue);
-        } else if (coin.equals("nickel")){
-                currentAmount = currentAmount.add(nickelValue);
-        } else if (coin.equals("quarter")){
-                currentAmount = currentAmount.add(quarterValue);
-            } else{
-                return coin;
-            }
+        if (coin.equals("dime")) {
+            currentAmount = currentAmount.add(dimeValue);
+        } else if (coin.equals("nickel")) {
+            currentAmount = currentAmount.add(nickelValue);
+        } else if (coin.equals("quarter")) {
+            currentAmount = currentAmount.add(quarterValue);
+        } else {
+            return coin;
+        }
     }
 
-}
+
