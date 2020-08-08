b14a766a36dc3fbd70687154b688da0241152eb4
Rchambe9 <Rchambe9@gmail.com>
2020-08-08 11:40:03 -0400

    Created test and coin accepts

--- src/main/java/com/fizzbuzzcola/vendingmachine/VendingMachine.java
+++ src/main/java/com/fizzbuzzcola/vendingmachine/VendingMachine.java
@@ -1,7 +1,37 @@
 package com.fizzbuzzcola.vendingmachine;
 
+import java.math.BigDecimal;
+
 public class VendingMachine {
+//    variable
+    private BigDecimal currentAmount;
+//    constructor
+    public VendingMachine(BigDecimal currentAmount) {
+        this.currentAmount = currentAmount;
+    }
+//    getter and setter
+    public BigDecimal getCurrentAmount(){
+        return currentAmount;
+    }
+    public void setCurrentAmount(BigDecimal currentAmount) {
+        this.currentAmount = currentAmount;
+    }
+
     public String displayInsertCoins() {
         return "Insert Coins";
     }
+
+    public void acceptCoin(String coin) {
+        BigDecimal dimeValue = new BigDecimal("0.10");
+        BigDecimal nickelValue = new BigDecimal("0.05");
+        BigDecimal quarterValue = new BigDecimal("0.25");
+            if (coin.equals("dime")){
+                currentAmount = currentAmount.add(dimeValue);
+        } else if (coin.equals("nickel")){
+                currentAmount = currentAmount.add(nickelValue);
+        } else if (coin.equals("quarter")){
+                currentAmount = currentAmount.add(quarterValue);
+            }
+    }
+
 }
--- src/test/java/com/fizzbuzzcola/vendingmachine/VendingMachineTest.java
+++ src/test/java/com/fizzbuzzcola/vendingmachine/VendingMachineTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.math.BigDecimal;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -15,14 +16,33 @@ public class VendingMachineTest {
 
     @Test
     public void vendingMachineShouldDisplayInsertCoins() {
-        VendingMachine underTest = new VendingMachine();
+        VendingMachine underTest = new VendingMachine(BigDecimal.ZERO);
 
        String message = underTest.displayInsertCoins();
         assertEquals("Insert Coins", message);
     }
 
-
-
+    @Test
+    public void vendingMachineShouldAcceptCoins(){
+        VendingMachine underTest = new VendingMachine(BigDecimal.ZERO);
+        underTest.acceptCoin("dime");
+        BigDecimal dimeValue = new BigDecimal("0.10");
+        assertEquals(dimeValue, underTest.getCurrentAmount());
+    }
+    @Test
+    public void vendingMachineShouldAcceptNickel(){
+        VendingMachine underTest = new VendingMachine(BigDecimal.ZERO);
+        underTest.acceptCoin("nickel");
+        BigDecimal nickelValue = new BigDecimal("0.05");
+        assertEquals(nickelValue, underTest.getCurrentAmount());
+    }
+    @Test
+    public void vendingMachineShouldAcceptQuarter(){
+        VendingMachine underTest = new VendingMachine(BigDecimal.ZERO);
+        underTest.acceptCoin("quarter");
+        BigDecimal quarterValue = new BigDecimal("0.25");
+        assertEquals(quarterValue, underTest.getCurrentAmount());
+    }
     }
 
 
